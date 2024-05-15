d4a9ca02be272fb08aa731395be8861dd5d45e4b
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-05-15 18:46:44 +0500

    home section in progress

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+// home slider
+let swiper = new Swiper(".homeSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".home-button-next",
+        prevEl: ".home-button-prev",
+    },
+});
+// home slider
