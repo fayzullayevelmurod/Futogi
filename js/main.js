f1b7b95ab3effd90b6b208fe8115b6c663d07869
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-05-22 00:28:10 +0500

    products desktop in progress

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// burger
+let header_burger = document.querySelector('.header_burger');
+let header_burger_btn = document.querySelector('.header_burger_btn');
+let header_menu = document.querySelector('.header_menu');
+
+header_burger.addEventListener('click', () => {
+  header_menu.classList.toggle('active');
+  header_burger_btn.classList.toggle('active');
+})
+// burger
+
 
 // home slider
 let swiper = new Swiper(".homeSwiper", {
