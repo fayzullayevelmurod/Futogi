f6195dcbc4150f1a82755087ee3fe6545549ece5
SardorQobilov <sardorqobilov555@gmail.com>
2024-06-21 09:17:08 +0500

    S

--- src/pages/MakingOrder.jsx
+++ src/pages/MakingOrder.jsx
@@ -1,4 +1,4 @@
-// import { useState, useContext } from "react";
+// import React, { useState, useContext } from "react";
 // import assets from "../assets";
 // import { OrderList } from "../components";
 // import {
@@ -17,20 +17,29 @@
 // import { BasketContext } from "../context/BasketContext";
 // import { useLocation, useNavigate } from "react-router-dom";
 
+// // Telefon raqamining tekshirilishi
 // const isValidPhoneNumber = (phoneNumber) =>
 //   phoneNumber && phoneNumber.length > 0;
+
+// // Foydalanuvchi ismi uchun tekshirilishi
 // const isValidUserName = (userName) => userName && userName.length > 0;
+
+// // Yetkazib berish uchun manzil tekshirilishi
 // const isValidAddress = (deliveryMethod, address, house, kvartira, etaj) =>
 //   deliveryMethod !== "delivery" || (address && house && kvartira && etaj);
+
+// // Yetkazib berish vaqti tekshirilishi
 // const isValidDeliveryTime = (deliveryTime, selectedOption, options) =>
 //   deliveryTime !== "pickup2" || options.includes(selectedOption);
 
+// // Vaqt diapazonini vaqtga o'girish
 // const convertTimeRangeToTime = (timeRange) => {
 //   const [startTime] = timeRange.split(" - ");
 //   return `${startTime}:00`;
 // };
 
 // export const MakingOrder = () => {
+//   // Komponentdagi holatlar va ma'lumotlar
 //   const [deliveryMethod, setDeliveryMethod] = useState("");
 //   const [deliveryTime, setDeliveryTime] = useState("");
 //   const [paymentMethod, setPaymentMethod] = useState("");
@@ -53,6 +62,7 @@
 //   const { personCount } = location.state;
 //   const navigate = useNavigate();
 
+//   // Yetkazib berish vaqtlari
 //   const options = [
 //     "18:00 - 18:30",
 //     "18:30 - 19:00",
@@ -64,15 +74,18 @@
 //     "21:30 - 22:00",
 //   ];
 
+//   // Vaqt diapazonini tanlash
 //   const handleOptionClick = (option) => {
 //     setSelectedOption(option);
 //     setShowOptions(false);
 //   };
 
+//   // Buyurtma ro'yxatini ko'rsatish
 //   const handleShowOrderList = () => {
 //     setShowOrderList(!showOrderList);
 //   };
 
+//   // Ma'lumotlarni tekshirish
 //   const handleValidation = () => {
 //     if (!isValidPhoneNumber(phoneNumber)) {
 //       toast.error("Пожалуйста, введите ваш номер телефона.");
@@ -99,6 +112,7 @@
 //     return true;
 //   };
 
+//   // Umumiy narxni hisoblash
 //   const calculateTotalPrice = () => {
 //     return basket.reduce((total, item) => {
 //       const itemTotal = item.price * item.count;
@@ -108,6 +122,8 @@
 //       return total + itemTotal + modsTotal;
 //     }, 0);
 //   };
+
+//   // Buyurtmani yuborish
 //   const handleSubmitOrder = async () => {
 //     setLoading(true);
 
@@ -157,7 +173,6 @@
 //       source: 1,
 //       comment: comment,
 //       promo: promoCode,
-//       // changeAmount
 //     };
 
 //     try {
@@ -174,16 +189,13 @@
 //         console.log("Order successfully created:", responseData);
 //         toast.success("Заказ успешно создан!");
 
-//         // responseData ni localStorage ga saqlash
 //         localStorage.setItem("orderData", JSON.stringify(responseData));
-//         navigate("/madeorder", {
-//           state: {
-//             deliveryAddress: `${address}, дом ${house}, квартира/офис ${kvartira}, этаж ${etaj}`,
-//           },
+//         navigate("/made-order", {
+//           state: { deliveryAddress: fullAddress },
 //         });
 //       } else {
 //         const errorData = await response.json();
-//         toast.error(errorData.detail);
+//         toast.error(errorData?.detail?.error?.message);
 //       }
 //     } catch (error) {
 //       toast.error("Сетевая ошибка. Попробуйте еще раз.");
@@ -191,7 +203,269 @@
 //       setLoading(false);
 //     }
 //   };
-//
+//   return (
+//     <>
+//       <div className="making_order">
+//         <div className="parent_sidebar">
+//           <div className="title_box">
+//             <h1 className="media_title title">ОФОРМЛЕНИЕ ЗАКАЗА</h1>
+//             <div className="top_gradient">
+//               <img src={assets.topGradient} alt="top gradient" />
+//             </div>
+//           </div>
+//           <div className="sidebars">
+//             <div className="left_box">
+//               <div className="form_boxes">
+//                 <FormSection title="Персональные данные">
+//                   <form className="form first-form">
+//                     <div className="form_item">
+//                       <label className="form_label">Номер телефона:</label>
+//                       <PhoneInput
+//                         value={phoneNumber}
+//                         onChange={setPhoneNumber}
+//                         placeholder="+7 (___) ___-__-__"
+//                       />
+//                     </div>
+//                     <FormItem
+//                       label="Ваше имя:"
+//                       type="text"
+//                       name="name"
+//                       value={userName}
+//                       onChange={(e) => setUserName(e.target.value)}
+//                     />
+//                   </form>
+//                 </FormSection>
+//                 <FormSection title="Адрес получения">
+//                   <form className="checked_box">
+//                     <div className="form">
+//                       <RadioItem
+//                         id="delivery"
+//                         name="deliveryMethod"
+//                         label="Доставка"
+//                         checked={deliveryMethod === "delivery"}
+//                         onChange={() => setDeliveryMethod("delivery")}
+//                       />
+//                       <RadioItem
+//                         id="pickup"
+//                         name="deliveryMethod"
+//                         label="Самовывоз"
+//                         checked={deliveryMethod === "pickup"}
+//                         onChange={() => setDeliveryMethod("pickup")}
+//                       />
+//                       {deliveryMethod === "delivery" && (
+//                         <div className="top_child-form">
+//                           <FormItem
+//                             label="Населенный пункт, улица:"
+//                             type="text"
+//                             name="address"
+//                             className="full"
+//                             value={address}
+//                             onChange={(e) => setAddress(e.target.value)}
+//                           />
+//                           <FormSection className="flex_box full">
+//                             <FormItem
+//                               label="Дом"
+//                               type="text"
+//                               className="house"
+//                               value={house}
+//                               onChange={(e) => setHouse(e.target.value)}
+//                             />
+//                             <FormItem
+//                               label="Квартира/офис"
+//                               type="text"
+//                               className="kvartira"
+//                               value={kvartira}
+//                               onChange={(e) => setKvartira(e.target.value)}
+//                             />
+//                             <FormItem
+//                               label="Этаж"
+//                               type="number"
+//                               className="etaj"
+//                               value={etaj}
+//                               onChange={(e) => setEtaj(e.target.value)}
+//                             />
+//                           </FormSection>
+//                         </div>
+//                       )}
+//                       {deliveryMethod === "pickup" && (
+//                         <RadioItem2
+//                           id="vladimirAddress"
+//                           label="г. Владимир, ул. Пушкина, д. 8"
+//                           checked={true}
+//                         />
+//                       )}
+//                     </div>
+//                   </form>
+//                   <Button className="add__cart-btn h-50" label="Подтвердить" />
+//                 </FormSection>
+//                 <FormSection>
+//                   <div className="comments_box">
+//                     <FormTextArea
+//                       label="Комментарий курьеру:"
+//                       value={comment}
+//                       onChange={(e) => setComment(e.target.value)}
+//                     />
+//                   </div>
+//                 </FormSection>
+//                 <FormSection>
+//                   <form className="form checked_box check_form-box">
+//                     <RadioItem
+//                       id="delivery2"
+//                       name="deliveryTime"
+//                       label="Как можно скорее"
+//                       checked={deliveryTime === "delivery2"}
+//                       onChange={() => setDeliveryTime("delivery2")}
+//                     />
+//                     <RadioItem
+//                       id="pickup2"
+//                       name="deliveryTime"
+//                       label="К точному времени"
+//                       checked={deliveryTime === "pickup2"}
+//                       onChange={() => setDeliveryTime("pickup2")}
+//                     />
+//                     {deliveryTime === "pickup2" && (
+//                       <FormSection className="delivery_time">
+//                         <div className="form_item">
+//                           <label className="form_label">
+//                             Укажите время доставки
+//                           </label>
+//                           <div className="select__box">
+//                             <input
+//                               type="text"
+//                               value={selectedOption}
+//                               readOnly
+//                               className="form_input select__input"
+//                               onClick={() => setShowOptions(!showOptions)}
+//                             />
+//                             {showOptions && (
+//                               <div className="option__box">
+//                                 {options.map((option, index) => (
+//                                   <div
+//                                     key={index}
+//                                     className="option"
+//                                     onClick={() => handleOptionClick(option)}
+//                                   >
+//                                     {option}
+//                                   </div>
+//                                 ))}
+//                               </div>
+//                             )}
+//                           </div>
+//                         </div>
+//                       </FormSection>
+//                     )}
+//                   </form>
+//                 </FormSection>
+//                 <div className="form_box promocode_box">
+//                   <FormSection>
+//                     <PromocodeInput
+//                       value={promoCode}
+//                       onChange={(e) => setPromoCode(e.target.value)}
+//                     />
+//                   </FormSection>
+//                 </div>
+//                 <div className="payment_method">
+//                   <FormSection title="Способ оплаты">
+//                     <form className="check_form-box">
+//                       <div className="checked_box">
+//                         <RadioItem
+//                           id="cash"
+//                           name="paymentMethod"
+//                           label="Оплата наличными при получении"
+//                           checked={paymentMethod === "cash"}
+//                           onChange={() => setPaymentMethod("cash")}
+//                         />
+//                       </div>
+//                       {paymentMethod === "cash" && (
+//                         <div className="check_form-box checked_box sm-radio">
+//                           <CheckboxItem
+//                             id="noChange"
+//                             label="Без сдачи"
+//                             checked={noChange}
+//                             onChange={() => setNoChange(!noChange)}
+//                           />
+//                         </div>
+//                       )}
+//                       {!noChange && paymentMethod === "cash" && (
+//                         <div className="ways_pay">
+//                           <FormItem
+//                             label="Укажите с какой суммы подготовить сдачу"
+//                             type="number"
+//                             placeholder="1000Р"
+//                             value={changeAmount}
+//                             onChange={(e) => setChangeAmount(e.target.value)}
+//                           />
+//                           <div className="example_prices">
+//                             <span onClick={() => setChangeAmount("1000")}>
+//                               1000
+//                             </span>
+//                             <span onClick={() => setChangeAmount("2000")}>
+//                               2000
+//                             </span>
+//                             <span onClick={() => setChangeAmount("5000")}>
+//                               5000
+//                             </span>
+//                           </div>
+//                         </div>
+//                       )}
+//                       <div className="checked_box">
+//                         <RadioItem
+//                           id="card"
+//                           name="paymentMethod"
+//                           label="Оплата картой при получении"
+//                           checked={paymentMethod === "card"}
+//                           onChange={() => setPaymentMethod("card")}
+//                         />
+//                       </div>
+//                     </form>
+//                   </FormSection>
+//                 </div>
+//                 <Button
+//                   label={`${loading ? "Загрузка..." : "Оформить заказ"}`}
+//                   className="send_btn add__cart-btn"
+//                   onClick={handleSubmitOrder}
+//                 />
+//               </div>
+//             </div>
+//             <div className="right_box">
+//               <button className="show_order" onClick={handleShowOrderList}>
+//                 Показать заказ
+//               </button>
+//               <div className="desktop_order-list">
+//                 <OrderList
+//                   deliveryAddress={
+//                     deliveryMethod === "delivery"
+//                       ? `${address}, дом ${house}, квартира/офис ${kvartira}, этаж ${etaj}`
+//                       : "г. Владимир, ул. Пушкина, д. 8"
+//                   }
+//                 />
+//               </div>
+//               <div
+//                 className={`media_order-list ${showOrderList ? "show" : ""}`}
+//               >
+//                 <OrderList
+//                   deliveryAddress={
+//                     deliveryMethod === "delivery"
+//                       ? `${address}, дом ${house}, квартира/офис ${kvartira}, этаж ${etaj}`
+//                       : "г. Владимир, ул. Пушкина, д. 8"
+//                   }
+//                 />
+//               </div>
+//               <Button
+//                 label={`${loading ? "Загрузка..." : "Оформить заказ"}`}
+//                 className="button send_btn add__cart-btn"
+//                 onClick={handleSubmitOrder}
+//               />
+//             </div>
+//           </div>
+//         </div>
+//         <div className="bottom_gradient">
+//           <img src={assets.gradientBig} alt="top gradient" />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
 import React, { useState, useContext } from "react";
 import assets from "../assets";
@@ -304,6 +578,14 @@ export const MakingOrder = () => {
       toast.error("Выберите допустимое время.");
       return false;
     }
+
+    const totalPrice = calculateTotalPrice();
+    if (changeAmount && parseFloat(changeAmount) < totalPrice) {
+      toast.error(`Сумма не может быть меньше ${totalPrice} штук.`);
+      document.querySelector("input[name='changeAmount']").focus();
+      return false;
+    }
+
     return true;
   };
 
@@ -327,34 +609,14 @@ export const MakingOrder = () => {
       return;
     }
 
+    // Umumiy narxni hisoblash va kiritish
+    const totalPrice = calculateTotalPrice();
+
+    // Change the address and isPickup fields based on the delivery method
     const fullAddress =
       deliveryMethod === "delivery"
         ? `${address}, дом ${house}, квартира/офис ${kvartira}, этаж ${etaj}`
-        : "г. Владимир, ул. Пушкина, д. 8";
-
-    const totalPrice = calculateTotalPrice();
-
-    if (changeAmount && changeAmount < totalPrice) {
-      toast.error(`Сумма не может быть меньше ${totalPrice} штук.`);
-      setLoading(false);
-      return;
-    }
-
-    const nomenclature = basket.map((item) => ({
-      id: item.id,
-      count: item.count,
-      modifiers: item.mods
-        ? item.mods.map((mod) => ({
-            id: mod.id,
-            count: mod.count || 1,
-          }))
-        : [],
-    }));
-
-    const orderTime =
-      deliveryTime === "pickup2"
-        ? convertTimeRangeToTime(selectedOption)
-        : "now";
+        : null;
 
     const orderData = {
       name: userName,
@@ -363,11 +625,26 @@ export const MakingOrder = () => {
       paymentType: paymentMethod,
       persons: personCount,
       phone: phoneNumber,
-      nomenclature,
-      time: orderTime,
+      nomenclature: basket.map((item) => ({
+        id: item.id,
+        count: item.count,
+        modifiers: item.mods
+          ? item.mods.map((mod) => ({
+              id: mod.id,
+              count: mod.count || 1,
+            }))
+          : [],
+      })),
+      time:
+        deliveryTime === "pickup2"
+          ? convertTimeRangeToTime(selectedOption)
+          : "now",
       source: 1,
       comment: comment,
       promo: promoCode,
+      isPickup: deliveryMethod === "pickup",
+      totalSum: totalPrice,
+      changeAmount: paymentMethod === "cash" ? changeAmount : null, // Add changeAmount if payment method is cash
     };
 
     try {
@@ -398,6 +675,7 @@ export const MakingOrder = () => {
       setLoading(false);
     }
   };
+
   return (
     <>
       <div className="making_order">
@@ -587,6 +865,7 @@ export const MakingOrder = () => {
                             label="Укажите с какой суммы подготовить сдачу"
                             type="number"
                             placeholder="1000Р"
+                            name="changeAmount"
                             value={changeAmount}
                             onChange={(e) => setChangeAmount(e.target.value)}
                           />
